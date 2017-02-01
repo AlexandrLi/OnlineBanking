60e4c11f13f56d873bce21985524ef5a4d8c6350
Alexandr.li <muzikant1990@gmail.com>
2017-02-01 23:27:39 +0600

    Initial commit

---
+++ src/main/java/com/ali/userfront/domain/SavingsAccount.java
@@ -0,0 +1,52 @@
+package com.ali.userfront.domain;
+
+import java.math.BigDecimal;
+
+public class SavingsAccount {
+    private Long id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+
+    private List<SavingsTransaction> savingsTransactionList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public List<SavingsTransaction> getSavingsTransactionList() {
+        return savingsTransactionList;
+    }
+
+    public void setSavingsTransactionList(List<SavingsTransaction> savingsTransactionList) {
+        this.savingsTransactionList = savingsTransactionList;
+    }
+
+    @Override
+    public String toString() {
+        return "SavingsAccount{" +
+                "id=" + id +
+                ", accountNumber=" + accountNumber +
+                ", accountBalance=" + accountBalance +
+                '}';
+    }
+}
