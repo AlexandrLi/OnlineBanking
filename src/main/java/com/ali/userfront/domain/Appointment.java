c432f3dac1265b5bfd10c03f3d48ea19e1cc01d5
Alexandr_Li <Alexandr_li@epam.com>
2017-02-02 19:24:54 +0600

    Add Appointment.class

---
+++ src/main/java/com/ali/userfront/domain/Appointment.java
@@ -0,0 +1,71 @@
+package com.ali.userfront.domain;
+
+import java.util.Date;
+
+public class Appointment {
+    private Long id;
+    private Date date;
+    private String location;
+    private String description;
+    private boolean confirmed;
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "id=" + id +
+                ", date=" + date +
+                ", location='" + location + '\'' +
+                ", description='" + description + '\'' +
+                ", confirmed=" + confirmed +
+                '}';
+    }
+}
